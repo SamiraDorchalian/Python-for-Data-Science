e45ebf2c3fe404e8cb6bb00fee763cd889bbe05e
Samira Dorchalian <samiradorchalain1998@gmail.com>
2025-11-08 18:32:44 +0330

    install numpy_library

---
+++ numPy_libaray.py
@@ -0,0 +1,53 @@
+import numpy as np
+                                                        ## vectore ##
+# list_1 = [1, 2, 3, 4]
+# print(list_1)
+
+# conversion_list_to_array_One_dimensional = np.array(list_1)
+# print(conversion_list_to_array_One_dimensional)
+
+                                                        ## Matrix ##
+
+# list_2 = [[1,2,3], [4,5,6]]
+# print(list_2)
+
+# conversion_list_to_array_Two_dimensional = np.array(list_2)
+# print(conversion_list_to_array_Two_dimensional)
+
+                                                        ## arange ##
+# arange_vslue = np.arange(0, 10)
+# print(arange_vslue)
+
+# arange_vslue = np.arange(0, 20, 5)
+# print(arange_vslue)
+
+                                                        ## zeros Matrix
+# make_matrix_zeros = np.zeros(5)
+# print(make_matrix_zeros)
+
+# make_matrix_zeros = np.zeros((5, 5))
+# print(make_matrix_zeros)
+
+                                                        ## linspace ##
+# linspace_number = np.linspace(0, 100, 6)
+# print(linspace_number)
+
+                                                        ## eye - Diagonal matrix ##
+# diagonal_matrix = np.eye(5)
+# print(diagonal_matrix)
+
+                                                        ## random and rand ##
+# random_number = np.random.rand(2)
+# print(random_number)
+
+# random_number = np.random.rand(2, 2)
+# print(random_number)
+
+                                                        ## random and randint ##
+# random_int = np.random.randint(0, 100)
+# print(random_int)
+
+# random_int = np.random.randint(0, 100, 5)
+# print(random_int)
+
+
